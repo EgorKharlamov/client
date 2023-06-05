b870894b70a0620a87c019d83fcce1733f1b36e0
EgorKharlamov <EgorKharlamov9338@gmail.com>
2023-06-06 02:40:57 +0700

    remove console log

--- store/usersStore.ts
+++ store/usersStore.ts
@@ -15,12 +15,10 @@ export const useUsersStore = defineStore("users", {
   actions: {
     async loadUsers(params = DEFAULT_PARAMS) {
       const res = await useApi(Method.GET, userEndpoint.getUsers(), { params });
-      console.log("res");
       this.users = res?.value?.data;
       this.count = res?.value?.count;
     },
     async deleteUserById(id: number) {
-      console.log("resdel");
       await useApi(Method.DELETE, userEndpoint.deleteUserById(id));
     },
   },
